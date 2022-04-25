60bf698f9c546bd1748082a76b51d153b4757751
Gedewon <gedewon.hayle.9@gmail.com>
2022-04-25 23:01:51 +0300

    linter fix

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-import { reloadBook as initialize, render } from "./modules/helperElement.js";
-import { DateTime } from "./modules/luxon.js";
-const timeDate = document.querySelector("#time-date");
+import { reloadBook as initialize, render } from './modules/helperElement.js';
+import { DateTime } from './modules/luxon.js';
+
+const timeDate = document.querySelector('#time-date');
 
 setInterval(() => {
   timeDate.innerHTML = DateTime.now().toLocaleString(
-    DateTime.DATETIME_FULL_WITH_SECONDS
+    DateTime.DATETIME_FULL_WITH_SECONDS,
   );
 }, 60);
 
 initialize();
 
-document.querySelector("nav").addEventListener("click", (e) => {
+document.querySelector('nav').addEventListener('click', (e) => {
   switch (e.target.id) {
-    case "list":
-      render("list");
+    case 'list':
+      render('list');
       break;
-    case "newBook":
-      render("newBook");
+    case 'newBook':
+      render('newBook');
       break;
-    case "contact":
-      render("contact");
+    case 'contact':
+      render('contact');
       break;
     default:
       break;
--- modules/helperElement.js
+++ modules/helperElement.js
@@ -1,16 +1,16 @@
-import { screenElement } from "./ScreenElement.js";
-import BooksShelf from "./BooksShelf.js";
-const allAddedBooks = new BooksShelf();
-const booksContainer = document.getElementById("books-dynamic-container");
-const title = document.getElementById("title");
+import BooksShelf from './BooksShelf.js';
 
-export function removeBooks() {
-  document.querySelectorAll(".remove-btn").forEach((element) => {
-    let index = element.id;
+const allAddedBooks = new BooksShelf();
+const booksContainer = document.getElementById('books-dynamic-container');
+const title = document.getElementById('title');
+
+function removeBooks() {
+  document.querySelectorAll('.remove-btn').forEach((element) => {
+    const index = element.id;
     if (!element) return;
-    element.addEventListener("click", () => {
+    element.addEventListener('click', () => {
       allAddedBooks.removeBook(index);
-      reloadBook();
+      window.location.reload();
     });
   });
 }
@@ -20,26 +20,26 @@ export function reloadBook() {
     .map(
       (
         bookItem,
-        index
+        index,
       ) => `<div class="book-item"  ><p class='title-author'><strong>"${bookItem.titleInput}" by ${bookItem.author}.</strong></p>
               <button class='remove-btn' id="${index}">Remove</button>
-              </div>`
+              </div>`,
     )
-    .join("");
+    .join('');
   booksContainer.innerHTML = listOfbooks;
-  removeBooks();
   if (allAddedBooks.books.length === 0) {
-    booksContainer.style.cssText = "border: none;";
+    booksContainer.style.cssText = 'border: none;';
   }
+  removeBooks();
   return listOfbooks;
 }
 
 function attach() {
-  const titleInput = document.getElementById("title-input");
-  const author = document.getElementById("author-input");
-  const addBookForm = document.getElementById("form-add-book");
+  const titleInput = document.getElementById('title-input');
+  const author = document.getElementById('author-input');
+  const addBookForm = document.getElementById('form-add-book');
 
-  addBookForm.addEventListener("submit", (event) => {
+  addBookForm.addEventListener('submit', (event) => {
     // function submitForm(event) {
     event.preventDefault();
     const newBook = {
@@ -48,34 +48,56 @@ function attach() {
     };
 
     allAddedBooks.addBook(newBook);
-    titleInput.value = "";
-    author.value = "";
+    titleInput.value = '';
+    author.value = '';
     reloadBook();
   });
-  //   const removeBooks = document.querySelectorAll(".remove-btn");
-  //   removeBooks.forEach((element) => {
-  //     let index = element.target.id;
-  //     element.addEventListener("click", () => {
-  //       console.log("hi");
-  //     });
-  //   });
 }
 // eslint-disable-next-line no-unused-vars
 export const removeBook = (bookIndex) => {
-  console.log("hi");
   allAddedBooks.removeBook(bookIndex);
   reloadBook();
 };
 
+const screenElement = {
+  list: {
+    title: 'All Awesome books',
+    content() {
+      return reloadBook();
+    },
+  },
+  newBook: {
+    title: 'Add a new book',
+    content: `<div class="section-con add-book">
+      <form method="post" id="form-add-book">
+        <input type="text" placeholder="Title" id="title-input" />
+        <input type="text" placeholder="Author" id="author-input" />
+        <button class="add-btn"  type="submit">Add</button>
+      </form>
+    </div>`,
+  },
+  contact: {
+    title: 'Contact information',
+    content: `<article class="contact">
+          <p>Do have any questions or you just want to say "Hello"?
+           </p>
+           <p>You can reach out to us!</p>
+           <ul>
+           <li>our e-mail: mail@mail.com</li>
+           <li>Our phone number: 0043586534422</li>
+           <li>Our address: Streetname 22, 84503 City,Country</li>
+           </ul>
+      </article > `,
+  },
+};
+
 export function render(renderAt) {
   const { content } = screenElement[renderAt];
-  booksContainer.innerHTML =
-    typeof content === "function" ? content() : content;
+  booksContainer.innerHTML = typeof content === 'function' ? content() : content;
   title.innerHTML = screenElement[renderAt].title;
 
-  booksContainer.style.cssText =
-    renderAt !== "list" ? "border: none;" : "border: 3px solid black;";
-  if (renderAt === "newBook") {
+  booksContainer.style.cssText = renderAt !== 'list' ? 'border: none;' : 'border: 3px solid black;';
+  if (renderAt === 'newBook') {
     attach();
   }
 }
